9440da709599469c5f9217f636ea1badbc17c54b
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-12-18 11:19:02 +0530

    Added Top Banner

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopBannerComponent } from './top-banner/top-banner.component';
 
 
 
@@ -7,6 +8,7 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: []
 })
 export class ComponentsModule { }
